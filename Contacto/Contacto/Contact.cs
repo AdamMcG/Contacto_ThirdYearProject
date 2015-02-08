cf76ad30c6982b2e93f56a6a83249bd1413ff7a7
AdamMcGivern <adammcgivern@outlook.com>
2015-02-08 23:15:14 +0000

    Added a Contact model class, addContact View and AddContactViewModel

---
+++ Contacto/Contacto/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacto
+{
+    class ContactModel
+    {
+        private string _firstName;
+        public string firstName
+        {
+            get { return _firstName; }
+            set { 
+
+            
+            }
+        }
+        private string _lastName;
+        public string lastName
+        {
+            get { return _lastName; }
+            set { }
+        
+        }
+        private string _phoneNumber;
+        public string phoneNumber
+        {
+            get { return phoneNumber; }
+            set { }
+        }
+    
+    }
+}

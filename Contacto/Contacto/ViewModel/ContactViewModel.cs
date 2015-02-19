c686aa1f2d8200e950763e64579e9278d6212c17
AdamMcGivern <adammcgivern@outlook.com>
2015-02-19 23:06:17 +0000

    Altered classes slightly- no real change.

--- Contacto/Contacto/ViewModel/ContactViewModel.cs
+++ Contacto/Contacto/ViewModel/ContactViewModel.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using Contacto.Model;
 using System.Threading.Tasks;
 
 namespace Contacto.ViewModel
 {
-    //This class is used to hold the business logic of the "add a contact" feature. 
+    //This class is used to hold the business logic of the "contact" feature. 
     //The add, update and delete methods should be in this class. 
     class ContactViewModel: INotifyPropertyChanged
     {
-        public ContactViewModel(){ 
+        ObservableCollection<Contact> myContacts;
+
+        public ContactViewModel(Contact aContact){
+            myContacts.Add(aContact);
+        }
+
+        public void addContact(){
         
         }
 
+        public void updateContact(){ 
+        
+        }
+        public void deleteContact()
+        { 
+        
+        }
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged(String propertyName)
         {
